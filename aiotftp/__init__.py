7da0b2f160921a36fc522103f25c456d03ba76a7
Terry Kerr <terry@dtkerr.ca>
2017-07-12 13:17:41 -0400

    export TftpProtocol

--- aiotftp/__init__.py
+++ aiotftp/__init__.py
@@ -3,3 +3,4 @@ from aiotftp.exceptions import PacketError
 from aiotftp.packet import create_packet
 from aiotftp.packet import parse_packet
 from aiotftp.router_base import TftpRouter
+from aiotftp.server_protocol import TftpProtocol
