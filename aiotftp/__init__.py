28407fa0bdf7cec3b04a4ed5eeaa71915670903a
Simon Gomizelj <simon@vodik.xyz>
2017-07-17 18:12:16 -0400

    Wrap tftp server creation

--- aiotftp/__init__.py
+++ aiotftp/__init__.py
@@ -1,6 +1,18 @@
-from aiotftp.exceptions import AioTftpError
-from aiotftp.exceptions import PacketError
-from aiotftp.packet import create_packet
-from aiotftp.packet import parse_packet
-from aiotftp.router_base import TftpRouter
-from aiotftp.server_protocol import TftpProtocol
+import asyncio
+
+from .exceptions import AioTftpError
+from .exceptions import PacketError
+from .packet import create_packet
+from .packet import parse_packet
+from .router_base import TftpRouter
+from .server_protocol import TftpProtocol
+
+
+def create_tftp_server(tftp_factory, *, loop=None, local_addr=None):
+    if not loop:
+        loop = asyncio.get_event_loop()
+
+    return loop.create_datagram_endpoint(
+        lambda: TftpProtocol(tftp_factory()),
+        local_addr=local_addr or ('0.0.0.0', 69)
+    )
