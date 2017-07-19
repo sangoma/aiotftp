7d3ba574b81aeebeeb9c1c6869e48d7170e99076
Terry Kerr <terry@dtkerr.ca>
2017-07-19 15:07:12 -0400

    add kwargs for the TftpProtocol instantiation in `create_tftp_server`

--- aiotftp/__init__.py
+++ aiotftp/__init__.py
@@ -8,11 +8,11 @@ from .router_base import TftpRouter
 from .server_protocol import TftpProtocol
 
 
-def create_tftp_server(tftp_factory, *, loop=None, local_addr=None):
+def create_tftp_server(tftp_factory, *, loop=None, local_addr=None, **kwargs):
     if not loop:
         loop = asyncio.get_event_loop()
 
     return loop.create_datagram_endpoint(
-        lambda: TftpProtocol(tftp_factory()),
+        lambda: TftpProtocol(tftp_factory(), **kwargs),
         local_addr=local_addr or ('0.0.0.0', 69)
     )
