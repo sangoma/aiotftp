1b6879af7a432033e422fa3f1ca767589b5cd9ec
Simon Gomizelj <simon@vodik.xyz>
2018-07-25 20:17:11 -0400

    Fix example to properly handle uploads and downloads

--- example/server.py
+++ example/server.py
@@ -7,43 +7,36 @@ from aiotftp.opcode import Opcode
 
 
 class Router(TftpRouter):
-    def __init__(self, future):
-        self.future = future
-
-    @classmethod
-    def with_future(cls):
-        future = asyncio.Future()
-        return cls(future), future
-
     def rrq_recieved(self, packet, remote):
         try:
             return open(packet.filename, 'rb')
-        except FileNotFoundError:
+        except OSError:
             return create_packet(
                 Opcode.ERROR,
                 error_code=ErrorCode.FILENOTFOUND,
                 error_msg="not found")
 
-    def rrq_complete(self):
-        self.future.set_result(True)
+    def rrq_complete(self, filename, remote):
+        print(f"File retreived: {filename}")
 
     def wrq_recieved(self, packet, remote):
         return io.BytesIO()
 
-    def wrq_complete(self, buffer):
+    def wrq_complete(self, buffer, filename, remote):
         buffer.seek(0)
-        self.future.set_result(buffer.read())
+        with open(filename, 'wb') as fp:
+            fp.write(buffer.read())
+        print(f"File uploaded: {filename}")
 
 
-router, future = Router.with_future()
+router = Router()
 loop = asyncio.get_event_loop()
 listen = loop.create_datagram_endpoint(
     lambda: TftpProtocol(router), ("127.0.0.1", 69))
 transport, protocol = loop.run_until_complete(listen)
 
 try:
-    loop.run_until_complete(future)
-    print(f"result: '{future.result()}'")
+    loop.run_forever()
 finally:
     transport.close()
     loop.close()
