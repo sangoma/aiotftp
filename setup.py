b742f6b5a26efaf174874a8c423200331acaf0c1
Simon Gomizelj <simon@vodik.xyz>
2018-07-28 23:14:47 -0400

    Prepare v0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name='aiotftp',
-    version='0.1.0',
+    version='0.1.1',
     author='Terry Kerr',
     author_email='terry@dtkerr.ca',
     maintainer='Terry Kerr',
