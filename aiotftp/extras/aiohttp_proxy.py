98d3141a9afd20e253d97cfd86c04143ce1db3c2
Simon Gomizelj <simon@vodik.xyz>
2018-07-31 18:29:57 -0400

    Add extras module

---
+++ aiotftp/extras/aiohttp_proxy.py
@@ -0,0 +1,19 @@
+import aiohttp
+import aiotftp
+
+
+async def proxy_from_url(request, url):
+    transfer = aiotftp.StreamResponse()
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            assert resp.status == 200
+            await transfer.prepare(request)
+
+            while True:
+                chunk = await resp.content.read(8192)
+                if not chunk:
+                    break
+                await transfer.write(chunk)
+
+    return transfer
